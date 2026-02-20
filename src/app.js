860d3cec1f081aca298c4cac8a3f3e20696d4a80
Shambel Dechu <shambeldechu@gmail.com>
2026-02-20 23:07:10 +0300

    style: move the middleware below all routes

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 const port = 3333;
 app.use(express.json());
-app.use(errorMiddleware);
 
 app.get("/", (req, res)=>{
     res.send("Wel come to SPEC!")
 }
 )
 app.use("/users", userRoutes)
+app.use(errorMiddleware);
+
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 }
-    
 )
